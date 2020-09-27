bb7fd70af8df945f3f5a1997815e67d9a4cf41fa
coffee2142 <35357093+coffee2142@users.noreply.github.com>
2020-09-27 09:15:52 -0700

    Update meme.py
    Fixed encoding issues by fucking ignoring them

--- meme.py
+++ meme.py
@@ -1,7 +1,3 @@
-# make sure you have a folder called "done" in the current directory
-# just go to /.git/objects/, make folder called done, and run
-
-
 import os
 import subprocess
 from subprocess import PIPE
@@ -15,5 +11,8 @@ for d in dank:
         for zd in od:
                 p = subprocess.Popen(['git','cat-file','-p',str(sd+zd)],stdin=PIPE,stdout=PIPE,stderr=PIPE)
                 output, err = p.communicate()
-                open("done/"+str(sd+zd),"w").write(str(output))
-                print(sd+zd)
+                try:
+                    open("done/"+str(sd+zd),"w").write(str(output.decode('utf-8')))
+                    print(sd+zd)
+                except UnicodeDecodeError:
+                     print("Failed to decode")
